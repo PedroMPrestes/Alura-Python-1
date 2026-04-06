0ec202713f1a75bd2287d00698841410a29dfb11
Pedro_Prestes <pedro.menestrino@edu.pucrs.br>
2026-04-06 08:32:05 -0300

    Dicionarios

--- app.py
+++ app.py
@@ -1,58 +1,87 @@
 import os
-import subprocess
 
+restaurantes = [{'nome': 'Pizza', 'categoria': 'Italiana', 'ativo': False},
+                {'nome': 'Sushi', 'categoria': 'Japonesa', 'ativo': False},
+                {'nome': 'Hamburguer', 'categoria': 'Americana', 'ativo': True}]
 
-def introducao():
-    print('============= Sabor Express ===============')
+def exibir_nome_do_programa():
+    print("""
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
+""")
 
-
-def opcoes():
-    print('1. Cadastrar Restaurante')
-    print('2. Listar Restaurante')
-    print('3. Ativar Restaurante')
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurantes')
+    print('3. Ativar restaurante')
     print('4. Sair\n')
 
-
 def finalizar_app():
-    subprocess.call('cls', shell=True)
-    print('Encerrando o Aplicativo\n')
+    exibir_subtitulo('Finalizar app')
 
-
-def reboot_app():
-    print("Opção inválida")
-    input("Digite uma tecla para retornar ao menu inicial")
+def voltar_ao_menu_principal():
+    input('\nDigite uma tecla para voltar ao menu ')
     main()
 
+def opcao_invalida():
+    print('Opção inválida!\n')
+    voltar_ao_menu_principal()
+
+def exibir_subtitulo(texto):
+    os.system('cls')
+    print(texto)
+    print()
+
+def cadastrar_novo_restaurante():
+    exibir_subtitulo('Cadastro de novos restaurantes')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+    categoria_do_restaurante = input('Digite a categoria do restaurante que deseja cadastrar: ')
+    dados_restaurante = {'nome':nome_do_restaurante, 'categoria':categoria_do_restaurante, 'ativo':False}
+    restaurantes.append(dados_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
+    
+    voltar_ao_menu_principal()
+
+def listar_restaurantes():
+    exibir_subtitulo('Listando restaurantes')
+
+    for restaurante in restaurantes:
+        #print(f'- {restaurante}')
+        #print(f'- {restaurante["nome"]}')
+        nomerestaurante = restaurante['nome']
+        categorarestaurante = restaurante['categoria']
+        ativorestaurante = restaurante['ativo']
+        print(f'- {nomerestaurante} | {categorarestaurante}| {ativorestaurante}')        
+
+    voltar_ao_menu_principal()
 
 def escolher_opcao():
     try:
-        selected_choice = int(input('Escolha uma opção: '))
-        # selected_choice = int(selected_choice)
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        # opcao_escolhida = int(opcao_escolhida)
 
-        # print(selected_choice == 1)
-        # print(type(selected_choice))
-        # print(type(1))
-
-        if selected_choice == 1:
-            print('Cadastrar Restaurante: ')
-        elif selected_choice == 2:
-            print('Listar Restaurante')
-        elif selected_choice == 3:
-            print('Ativar Restaurante')
-        elif selected_choice == 4:
+        if opcao_escolhida == 1: 
+            cadastrar_novo_restaurante()
+        elif opcao_escolhida == 2: 
+            listar_restaurantes()
+        elif opcao_escolhida == 3: 
+            print('Ativar restaurante')
+        elif opcao_escolhida == 4: 
             finalizar_app()
-        else:
-            reboot_app()
+        else: 
+            opcao_invalida()
     except:
-        reboot_app()
-
+        opcao_invalida()
 
 def main():
-    subprocess.call('cls', shell=True)
-    introducao()
-    opcoes()
+    os.system('cls')
+    exibir_nome_do_programa()
+    exibir_opcoes()
     escolher_opcao()
-
 
 if __name__ == '__main__':
     main()
--- exercicios.py
+++ exercicios.py
@@ -28,3 +28,58 @@ print('O valor arredondado de pi é: {:.2f}'.format(pi))
 
 # Utilizando a função round()
 print('O valor arredondado de pi é:', round(pi, 2))
+
+
+#Listas e Tuplas
+# Criando uma lista de compras
+lista_de_compras = ["Maçã", "Banana", "Leite", "Pão", "Queijo"]
+
+# Adicionando um item à lista
+lista_de_compras.append("Ovos")
+
+# Removendo um item da lista
+lista_de_compras.remove("Banana")
+
+# Exibindo a lista
+print("Lista de Compras:")
+for item in lista_de_compras:
+    print("- " + item)
+
+
+# Definindo uma tupla de coordenadas geográficas
+coordenadas_gps = (40.7128, -74.0060)
+
+# Exibindo as coordenadas
+print("Coordenadas GPS:")
+print("Latitude:", coordenadas_gps[0])
+print("Longitude:", coordenadas_gps[1])
+
+
+#Loops
+#For
+numero = -1
+for _ in range(3):  # Supondo um número máximo de tentativas (3) arbitrário
+    numero = int(input("Digite um número positivo: "))
+    if numero > 0:
+        break
+
+print("Você digitou:", numero)
+
+
+#While
+numero = -1
+while numero <= 0:
+    numero = int(input("Digite um número positivo: "))
+
+print("Você digitou:", numero)
+
+#Try/Except
+encomendas = input("Digite os números das encomendas separados por vírgula: ").split(',')
+try:
+    for encomenda in encomendas:
+        print(int(encomenda))
+except ValueError:
+    print("Uma das entradas não é um número válido.")
+
+#dicinarios
+restaurantes = [{'nome': 'Pizza', 'categoria': 'Italiana', 'ativo': False}, {'nome': 'Sushi', 'categoria': 'Japonesa', 'ativo': False}]
